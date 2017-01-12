b020a04ade364c39daf2d78f6e06f934fecbb172
Namone <al2xnord@yahoo.com>
2017-01-12 15:07:25 -0700

    Adding TODO so I don't forget.

--- public/js/controllers/userController.js
+++ public/js/controllers/userController.js
@@ -1,7 +1,7 @@
 var userController = angular.module('userController', []);
 
 userController.controller('UserController', ['$scope', 'User', function($scope, User) {
-   
+    // TODO: FIX UNMET DEPENDENCY ERROR 
     $scope.onSubmit = function(username, password) {
         var user = User.newUser(username, password); // create new User object
         var response = User.createUser(user); // give it to the DB
